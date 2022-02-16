a3f0bb1fd1a6161a236864a2f46913237d14140e
Ethan-schonfeld <62685800+Ethan-schonfeld@users.noreply.github.com>
2022-02-15 22:44:47 -0800

    Add files via upload

---
+++ data_processing/image_main.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+local_directory = "/Users/ethanschonfeld/desktop/CS236G/data/train_images/"
+
+
+# In[4]:
+
+
+virtual_directory = "/home/ethanschonfeld/cs236g/vindr/train_images/"
+
+
+# In[ ]:
+
+
+vindr_directory = "/home/ethanschonfeld/cs236g/vindr/"
+
+
+# In[6]:
+
+
+from image_processing import *
+import os
+import torch
+
+
+# In[ ]:
+
+
+if torch.cuda.is_available():
+    directory = virtual_directory
+else:
+    directory = local_directory
+
+
+# In[ ]:
+
+
+annotations = pd.read_csv(vindr_directory+"annotations/train.csv")
+annotations.index = annotations.loc[:, "image_id"]
+
+
+# In[ ]:
+
+
+for file in os.listdir(directory):
+    print(file)
+    if file == ".DS_Store":
+        continue
+    npy_image = convert_dicom_to_npy(directory+file, save=False)
+    if npy_image is None:
+        continue
+    npy_image = crop_square(image=npy_image)
+    npy_image = downsample_square_image(image=npy_image, desired_size=128)
+    image_id = file[:-6]
+    if bool(set(annotations.loc[:, "lesion_type"]) & set(annotations.loc[image_id, "lesion_type"])):
+        print("To abnormal: ", image_id)
+        save_as_png(image=npy_image, file_directory="/home/ethanschonfeld/cs236g/training_data/abnormal/"+image_id+".png")
+    else:
+        save_as_png(image=npy_image, file_directory="/home/ethanschonfeld/cs236g/training_data/normal/"+image_id+".png")
+        print("To normal: ", image_id)
+
