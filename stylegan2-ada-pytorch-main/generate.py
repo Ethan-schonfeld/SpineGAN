848503cfe9dd0fe74076f68e4d7599e5898cbacf
Ethan-schonfeld <62685800+Ethan-schonfeld@users.noreply.github.com>
2022-03-02 15:02:59 -0800

    Update generate.py

--- stylegan2-ada-pytorch-main/generate.py
+++ stylegan2-ada-pytorch-main/generate.py
@@ -119,11 +119,7 @@ def generate_images(
         img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
         img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
         img = img[0].cpu().numpy().squeeze()
-        print(img)
-        print(np.shape(img))
-        print(img.dtype)
-        break
-        PIL.Image.fromarray(img[0].cpu().numpy()).save(f'{outdir}/seed{seed:04d}.png')
+        PIL.Image.fromarray(img).save(f'{outdir}/seed{seed:04d}.png')
         #PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
 
 
