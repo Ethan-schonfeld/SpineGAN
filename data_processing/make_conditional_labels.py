d01c49ee6cc014be44e5d3a4c809181b68e3da7f
Ethan-schonfeld <62685800+Ethan-schonfeld@users.noreply.github.com>
2022-03-05 17:02:57 -0800

    Update make_conditional_labels.py

--- data_processing/make_conditional_labels.py
+++ data_processing/make_conditional_labels.py
@@ -23,31 +23,36 @@ annotations.index = annotations.loc[:, "image_id"]
 labels = {}
 image_names = list(os.listdir(image_directory))
 os.chdir(image_directory)
+duplicates = []
 for picture_name in image_names:
-    print(picture_name[-4:])
     image_id = picture_name[:-4]
-    print(image_id)
     if picture_name[-4:] == ".png":
         condition = annotations.loc[image_id, "lesion_type"]
-        if condition == "No finding":
-            labels[picture_name] = 0
-        elif condition == "Disc space narrowing":
-            labels[picture_name] = 1
-        elif condition == "Foraminal stenosis":
-            labels[picture_name] = 2
-        elif condition == "Osteophytes":
-            labels[picture_name] = 3
-        elif condition == "Spondylolysthesis":
-            labels[picture_name] = 4
-        elif condition == "Surgical implant":
-            labels[picture_name] = 5
-        elif condition == "Vertebral collapse":
-            labels[picture_name] = 6
-        elif condition == "Other lesions":
-            labels[picture_name] = 7
-        else:
-            #os.remove(picture_name)
-            pass
+        try:
+            if condition == "No finding":
+                labels[picture_name] = 0
+            elif condition == "Disc space narrowing":
+                labels[picture_name] = 1
+            elif condition == "Foraminal stenosis":
+                labels[picture_name] = 2
+            elif condition == "Osteophytes":
+                labels[picture_name] = 3
+            elif condition == "Spondylolysthesis":
+                labels[picture_name] = 4
+            elif condition == "Surgical implant":
+                labels[picture_name] = 5
+            elif condition == "Vertebral collapse":
+                labels[picture_name] = 6
+            elif condition == "Other lesions":
+                labels[picture_name] = 7
+            else:
+                #os.remove(picture_name)
+                pass
+        except:
+            duplicates.append(image_id)
+duplicates = list(set(duplicates))
+print(duplicates)
+exit(0)
 
 # In[ ]:
 
