f8f63cb0bb8ff0ea9c34cb8fb8f54e7e3965f29e
Ethan-schonfeld <62685800+Ethan-schonfeld@users.noreply.github.com>
2022-03-05 16:42:30 -0800

    Update make_conditional_labels.py

--- data_processing/make_conditional_labels.py
+++ data_processing/make_conditional_labels.py
@@ -4,8 +4,8 @@
 # In[1]:
 
 
-image_directory = "/home/ethanschonfeld/cs236g/SpineGAN/stylegan2-ada-pytorch-main/abnormality_conditional_dataset/"
-
+image_directory = "/home/ethanschonfeld/cs236g/SpineGAN/stylegan2-ada-pytorch-main/conditional_dataset/"
+label_directory = "/home/ethanschonfeld/cs236g/vindr/annotations/train.csv"
 
 # In[3]:
 
@@ -13,14 +13,22 @@ image_directory = "/home/ethanschonfeld/cs236g/SpineGAN/stylegan2-ada-pytorch-ma
 import os
 import json
 import sys
+import pandas as pd
 
 # In[ ]:
 
+annotations = pd.read_csv(label_directory)
+annotations.index = annotations.loc[:, "image_id"]
 
 labels = {}
 image_names = list(os.listdir(image_directory))
 for picture_name in image_names:
     print(picture_name[-4:])
+    image_id = picture_name[:-4]
+    print(image_id)
+    exit(0)
+    if picture_name[-4:] == ".png":
+        condition = 
     exit(0)
     labels[picture_name] = 0
 
