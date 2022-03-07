42b9fad3c41a1d368c17e109c89002fe25713cf2
Ethan-schonfeld <62685800+Ethan-schonfeld@users.noreply.github.com>
2022-03-07 11:48:15 -0800

    Update training_loop.py

--- DL-stylegan2-ada-pytorch-main/training/training_loop.py
+++ DL-stylegan2-ada-pytorch-main/training/training_loop.py
@@ -288,8 +288,7 @@ def training_loop(
             with torch.autograd.profiler.record_function(phase.name + '_opt'):
                 for param in phase.module.parameters():
                     if param.grad is not None:
-                        misc.nan_to_num(param.grad, nan=0, posinf=1e5, neginf=-1e5, out=param.grad)
-                        param.grad = param.grad + torch.normal(mean=0., std=1.).cuda()
+                        param.grad = param.grad.add_(torch.randn(param.size()) * 0.1)
                 noise = sigma*Variable(torch.randn(doc_weights_shape).cuda())
                 nn.utils.clip_grad_norm_(phase.module.parameters(), max_norm=1.5, norm_type=2)
                 phase.opt.step()
