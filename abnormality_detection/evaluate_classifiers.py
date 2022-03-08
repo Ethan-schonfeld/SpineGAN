42ebefa6a71df42c99a99823aa4e3af442f65ae1
Ethan-schonfeld <62685800+Ethan-schonfeld@users.noreply.github.com>
2022-03-07 20:14:40 -0800

    Update evaluate_classifiers.py

--- abnormality_detection/evaluate_classifiers.py
+++ abnormality_detection/evaluate_classifiers.py
@@ -87,12 +87,12 @@ test_labels = np.asarray(test_labels)
 print("Loaded!")
 
 test_X = torch.empty(0, 3, 224, 224)
-    for image_number in range(test_images.shape[0]):
-        tensor = preprocess(Image.fromarray(test_images[image_number, :, :, :]))
-        tensor = tensor.unsqueeze(0)
-        test_X = torch.cat([test_X, tensor], dim=0)
-    if torch.cuda.is_available():
-        test_X.to('cuda')
+for image_number in range(test_images.shape[0]):
+    tensor = preprocess(Image.fromarray(test_images[image_number, :, :, :]))
+    tensor = tensor.unsqueeze(0)
+    test_X = torch.cat([test_X, tensor], dim=0)
+if torch.cuda.is_available():
+    test_X.to('cuda')
 test_labels = torch.Tensor(test_labels)
 
 
