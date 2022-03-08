4882de232c9770d5d771623cef105726b8c04124
Ethan-schonfeld <62685800+Ethan-schonfeld@users.noreply.github.com>
2022-03-07 16:02:25 -0800

    Update noise_multiplier_optimization.py

--- DL-stylegan2-ada-pytorch-main/noise_multiplier_optimization.py
+++ DL-stylegan2-ada-pytorch-main/noise_multiplier_optimization.py
@@ -23,7 +23,7 @@ from tensorflow_privacy.privacy.analysis import compute_dp_sgd_privacy
 # In[7]:
 
 
-x_noise = [(0.01)/1.5, 0.03/1.5, 0.06/1.5, 0.1/1.5, 0.2/1.5, 0.3/1.5, 0.4/1.5, 0.5/1.5, 0.6/1.5, 0.7/1.5, 0.8/1.5, 0.9/1.5, 1/1.5, 1.1/1.5, 1.2/1.5, 1.3/1.5, 1.4/1.5, 1.5/1.5, 1.6/1.5, 1.7/1.5, 1.8/1.5, 1.9/1.5, 2/1.5]
+x_noise = [0.03/1.5, 0.06/1.5, 0.1/1.5, 0.2/1.5, 0.3/1.5, 0.4/1.5, 0.5/1.5, 0.6/1.5, 0.7/1.5, 0.8/1.5, 0.9/1.5, 1/1.5, 1.1/1.5, 1.2/1.5, 1.3/1.5, 1.4/1.5, 1.5/1.5, 1.6/1.5, 1.7/1.5, 1.8/1.5, 1.9/1.5, 2/1.5]
 y_epsilon = []
 for noise in x_noise:
     epsilon = compute_dp_sgd_privacy.compute_dp_sgd_privacy(n=3773,
@@ -38,9 +38,9 @@ for noise in x_noise:
 # In[10]:
 
 
-plt.scatter(x_noise, np.log(y_epsilon))
+plt.scatter(x_noise, y_epsilon)
 plt.xlabel('Estimated Noise Multiplier (noise standard deviation/clipping norm)')
-plt.ylabel('Log Base 10 Epsilon')
+plt.ylabel('Epsilon')
 
 
 # In[ ]:
